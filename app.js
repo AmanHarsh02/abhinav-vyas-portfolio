fdc247bdf438f67350236dd03e717326a3aaa414
Aman Harsh <amanharsh0399@gmail.com>
2022-09-25 23:43:50 +0530

    first commit: developed landing page
    Developed landing page. Partially responsive.

---
+++ app.js
@@ -0,0 +1,11 @@
+const dynamicText = document.querySelector("#dynamic-text");
+
+var typed = new Typed(dynamicText, {
+    strings: ['a UX Designer', '<span class="red-color-span">an Interaction Designer</span>', '<span class="green-color-span">a Transcendental Meditator</span>', '<span class="yellow-color-span">a Good Observer!</span>', "a Student for Life"],
+    typeSpeed: 80,
+    backSpeed: 40,
+    showCursor: false,
+    backDelay: 1000,
+    loop: true
+}
+);
