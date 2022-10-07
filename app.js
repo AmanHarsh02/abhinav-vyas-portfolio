0c11f941aa22665a23a06dbd230b1923a3280f70
Aman Harsh <amanharsh0399@gmail.com>
2022-10-07 23:40:57 +0530

    feat: closing hamburger menu if clicked outside or any item is selected

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const dynamicText = document.querySelector("#dynamic-text");
+const menuBtn = document.querySelector("#menu-btn");
 
 var typed = new Typed(dynamicText, {
     strings: ['a UX Designer', '<span class="red-color-span">an Interaction Designer</span>', '<span class="green-color-span">a Transcendental Meditator</span>', '<span class="yellow-color-span">a Good Observer!</span>', "a Student for Life"],
@@ -9,3 +10,11 @@ var typed = new Typed(dynamicText, {
     loop: true
 }
 );
+
+function menuBtnClickHandler(event) {
+    if(event.target != menuBtn) {
+        menuBtn.checked = false;
+    }
+}
+
+window.addEventListener("click", menuBtnClickHandler);
