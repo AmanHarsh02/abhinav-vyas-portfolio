d73bccc92ffe3373580741fab8ad654a1f6a6adb
Aman Harsh <amanharsh0399@gmail.com>
2022-10-10 23:16:14 +0530

    fix: removed feature to hide scroll bar due to some issue

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
 const dynamicText = document.querySelector("#dynamic-text");
 const menuBtn = document.querySelector("#menu-btn");
 const menuItem = document.querySelectorAll(".link-nav");
-const navBar = document.querySelector(".navigation");
-let lastScrollY = window.scrollY;
-
-window.addEventListener("scroll", () => {
-    if(lastScrollY < window.scrollY) {
-        navBar.classList.add("nav-hidden");
-    } else {
-        navBar.classList.remove("nav-hidden");
-    }
-
-    lastScrollY = window.scrollY;
-});
 
 var typed = new Typed(dynamicText, {
     strings: ['a UX Designer', '<span class="red-color-span">an Interaction Designer</span>', '<span class="green-color-span">a Transcendental Meditator</span>', '<span class="yellow-color-span">a Good Observer!</span>', "a Student for Life"],
@@ -26,4 +14,4 @@ var typed = new Typed(dynamicText, {
 
 menuItem.forEach(items => items.addEventListener("click", () => {
     menuBtn.checked = false;
-}))
+}));
