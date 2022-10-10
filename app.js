66bd1fefecbcca67acb356c5b8f40045abafca6f
Aman Harsh <amanharsh0399@gmail.com>
2022-10-10 22:33:14 +0530

    feat: hiding nav bar on scroll

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const dynamicText = document.querySelector("#dynamic-text");
 const menuBtn = document.querySelector("#menu-btn");
 const menuItem = document.querySelectorAll(".link-nav");
+const navBar = document.querySelector(".navigation");
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+    if(lastScrollY < window.scrollY) {
+        navBar.classList.add("nav-hidden");
+    } else {
+        navBar.classList.remove("nav-hidden");
+    }
+
+    lastScrollY = window.scrollY;
+});
 
 var typed = new Typed(dynamicText, {
     strings: ['a UX Designer', '<span class="red-color-span">an Interaction Designer</span>', '<span class="green-color-span">a Transcendental Meditator</span>', '<span class="yellow-color-span">a Good Observer!</span>', "a Student for Life"],
